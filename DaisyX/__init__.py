5099307930e5c442742a04a1f2dcfa1b8e22f5fd
Inuka Asith <inukaasith7@gmail.com>
2021-05-11 09:44:37 +0530

    Create __init__.py

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -8,11 +8,10 @@
 
 
 import os
-import time
 import logging
 
 from pyrogram import Client
-from config import API_ID, API_HASH, SESSION, TOKEN
+from config import API_ID, API_HASH, STRING_SESSION, TOKEN
 
 
 StartTime = time.time()
@@ -23,4 +22,12 @@ daisy = Client(
       api_hash=API_HASH,
       sleep_threshold=180,
 )
+
+assist = Client(
+        "MyAssistant",
+        api_id=API_ID,
+        api_hash= API_HASH,
+        bot_token=TOKEN,
+        sleep_threshold=180,
+    )
 
