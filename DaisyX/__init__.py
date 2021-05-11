13d9066444f0cb4799ee0666e16db8830c230bfa
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-11 08:50:10 +0530

    Update __init__.py

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -1,1 +1,21 @@
+# (c) Copyright 21-22 by lucifeermorningstar
+#
+#
+#
+#
+#
 
+import os
+import logging
+
+from pyrogram import Client
+from config import API_ID, API_HASH, STRING_SESSION, TOKEN
+
+
+
+DaisyX = Client(
+      session_name=STRING_SESSION,
+      api_id=API_ID,
+      api_hash=API_HASH,
+      bot_token=TOKEN,
+)
