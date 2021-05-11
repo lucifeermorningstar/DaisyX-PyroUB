c78aef5e7ef65975c6776265cf66f7474c26a9ab
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-11 13:53:26 +0530

    Update __init__.py

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -58,18 +58,3 @@ async def get_self():
 Command = [".", "!","?"]'''
 
 
-# By Lodu Swonit
-
-def __list_all_modules():
-    from os.path import dirname, basename, isfile
-    import glob
-    mod_paths = glob.glob(dirname(__file__) + "/*.py")
-    return [
-        basename(f)[:-3] for f in mod_paths if isfile(f)
-        and f.endswith(".py")
-        and not f.endswith('__init__.py')
-        ]
-
-
-ALL_MODULES = sorted(__list_all_modules())
-__all__ = ALL_MODULES + ["ALL_MODULES"]
