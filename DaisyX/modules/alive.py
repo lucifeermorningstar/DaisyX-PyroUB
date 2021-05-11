62dfd9a0fc20f94f5e15dd16448afa3e8264b323
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-11 10:14:09 +0530

    Update alive.py

--- DaisyX/modules/alive.py
+++ DaisyX/modules/alive.py
@@ -1,1 +1,10 @@
+# Copyright (c) 21-22 by lucifeermorningstar@GitHub < https://github.com/lucifeermorningstar >
+# Alive For DaisyX Userbot
 
+from pyrogram import filters
+from DaisyX import daisy
+
+@daisy.on_message(filters.command("alive")) 
+async def alive(events):
+   chat = message.chat_id
+   await daisy.send_message(chat, "Master ! I am alive :)")
