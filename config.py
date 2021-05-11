610be0bcddda9cb7e9fa4ab681cdb2f20e0fe92d
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-11 13:14:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,8 @@ API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH") 
 TOKEN = os.environ.get("TOKEN") 
 SESSION = os.environ.get("STRING_SESSION") 
+DATABASE_URL = os.environ.get("DATABASE_URL") 
+
+# It's For Loading and Unloading Modules. 
+DLOAD = []
+DUNLOAD = []
