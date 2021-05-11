a1582baffacd11f0c12625752a31cd5815623240
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-11 13:53:55 +0530

    Update __init__.py

--- DaisyX/modules/__init__.py
+++ DaisyX/modules/__init__.py
@@ -1,5 +1,5 @@
 
-import importlib
+'''import importlib
 
 from config import DLOAD, DUNLOAD, log
 
@@ -33,4 +33,23 @@ def __list_all_modules():
 
 ALL_MODULES = sorted(__list_all_modules())
 log.info("DaisyX module loaded: %s", str(ALL_MODULES))
+__all__ = ALL_MODULES + ["ALL_MODULES"]'''
+
+
+# By Lodu Swonit
+
+def __list_all_modules():
+    from os.path import dirname, basename, isfile
+    import glob
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    return [
+        basename(f)[:-3] for f in mod_paths if isfile(f)
+        and f.endswith(".py")
+        and not f.endswith('__init__.py')
+        ]
+
+
+ALL_MODULES = sorted(__list_all_modules())
 __all__ = ALL_MODULES + ["ALL_MODULES"]
+
+
