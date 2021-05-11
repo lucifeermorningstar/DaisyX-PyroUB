583b941b9062a059018d7273c62294683637c3b6
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-11 09:03:48 +0530

    Update __init__.py

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -1,9 +1,11 @@
-# (c) Copyright 21-22 by Devil ( lucifeermorningstar ) 
-# All Written by @lucifeermorningstar.
-# For DaisyX Userbot. 
+# (c) Copyright 21-22 by lucifeermorningstar@GitHub, < https://GitHub.com/lucifeermorningstar >
 #
+# This file is part of < https://github.com/lucifeermorningstar/DaisyX-PyroUB > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/lucifeermorningstar/DaisyX-PyroUB/blob/main/LICENSE >
 #
-#
+# All rights reserved.
+
 
 import os
 import logging
